8343b1586c69b6779f076b5aba3de6ba22704cec
Nan Wu <ouchxp.me@gmail.com>
2020-05-06 22:06:47 +1200

    nodenv-nvmrc 1.0.2

--- nodenv-nvmrc.rb
+++ nodenv-nvmrc.rb
@@ -1,8 +1,8 @@
 class NodenvNvmrc < Formula
   desc "Pick a node version from .nvmrc"
   homepage "https://github.com/ouchxp/nodenv-nvmrc"
-  url "https://github.com/ouchxp/nodenv-nvmrc/archive/v1.0.1.tar.gz"
-  sha256 "8ada13ce9c94126459ba9edcc9992b6a736e610b84027cf23cf62a24aea88614"
+  url "https://github.com/ouchxp/nodenv-nvmrc/archive/v1.0.2.tar.gz"
+  sha256 "057f7c8b25bde3f37f3725573eb6d1edc241b4048fce02f4fa5779068b46070b"
   head "https://github.com/ouchxp/nodenv-nvmrc.git"
 
   depends_on "nodenv"
